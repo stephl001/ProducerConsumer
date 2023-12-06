062d70e840deb5792750caa4e81a984cba0731f7
Stephane Larocque <stephl001@gmail.com>
2023-12-06 19:32:37 +0000

    Project Creation

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Stack<string> stack = new Stack<string>();
+
+Task producerTask = Task.Run(Producer);
+Task consumerTask = Task.Run(Consumer);
+
+await Task.WhenAll(producerTask, consumerTask);
+
+Task Producer()
+{
+    while (true)
+    {
+        string? input = Console.ReadLine();
+
+        lock (stack)
+        {
+            stack.Push(input ?? "EMPTY");
+            Monitor.Pulse(stack);
+        }
+    }
+}
+
+Task Consumer()
+{
+    while (true)
+    {
+        lock (stack)
+        {
+            while (stack.Count == 0)
+                Monitor.Wait(stack);
+            
+            string output = stack.Pop();
+
+            Console.WriteLine($"Consumed: {output}");
+        }
+    }
+}
